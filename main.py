5ca599b2163dc310e4a6a8e1af893210e5c16435
Megham Garg <gargmegham3@gmail.com>
2024-02-26 15:08:06 +0530

    Add function to guess emails from company data

--- main.py
+++ main.py
@@ -166,3 +166,28 @@ def process_linkedin_htmls(file_name: str):
             "company_people": company_people_info,
         }
     json.dump(company_data, open("jsons/company_data.json", "w"))
+
+
+def guess_emails(company_data: dict):
+    emails = {}
+    for company_website in company_data:
+        suffix = company_website.replace("https://", "").replace("http://", "")
+        suffix = suffix.split("/")[0]
+        company_people = company_data[company_website]["company_people"]
+        if suffix.startswith("www."):
+            suffix = suffix[4:]
+        for person in company_people:
+            prefix = None
+            person_name = str(person[0]).strip().lower()
+            person_position = str(person[1]).strip().lower()
+            for word in person_name.split(" "):
+                if word.endswith("."):
+                    continue
+                if word == "linkedin":
+                    break
+                prefix = word
+                break
+            if prefix:
+                email = f"{prefix}@{suffix}"
+                emails[email] = [person_name, person_position]
+    return emails
