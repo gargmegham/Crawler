fac326420c48db983c1780e2fb2c1f9b1aa318ed
Megham Garg <gargmegham3@gmail.com>
2024-05-02 23:31:11 +0530

    Refactor main.py to add LinkedIn crawling functionality

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-from utils.clutch import crawl_companies
+from selenium import webdriver
+
+from utils.common import crawl_company
 
 if not os.path.exists("logs"):
     os.makedirs("logs")
@@ -27,5 +29,26 @@ client = MongoClient(uri, server_api=ServerApi("1"))
 if __name__ == "__main__":
     try:
         client.admin.command("ping")
+        clutch = client["clutch"]
+        companies = clutch["companies"]
+        driver = webdriver.Chrome()
+        for company in companies.find({
+            "website_link": {"$exists": True},
+            "linkedin_links": {"$exists": False}
+        }):
+            linkedin_links, emails, phone_numbers = crawl_company(company["website_link"], driver)
+            companies.update_one(
+                {"_id": company["_id"]},
+                {
+                    "$set": {
+                        "linkedin_links": linkedin_links,
+                        "emails": emails,
+                        "phone_numbers": phone_numbers,
+                    }
+                },
+            )
     except Exception as e:
         logging.error(f"Error connecting to MongoDB: {str(e)}", exc_info=True)
+    finally:
+        driver.quit()
+        client.close()
