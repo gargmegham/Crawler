6800930a4c065524bb055d2d21cd0bd11b29e1de
Megham Garg <gargmegham3@gmail.com>
2024-05-02 13:14:46 +0530

    Add support for selenium-wire in pyproject.toml

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import configparser
 import logging
+import os
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 logging.basicConfig(
     level=logging.ERROR,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     filemode="a",
     filename="logs/mongodb.log",
 )
-
 config = configparser.ConfigParser()
-config.read('secrets/config.ini')
-
-username = config['mongodb']['username']
-password = config['mongodb']['password']
-app = config['mongodb']['app']
+config.read("secrets/config.ini")
+username = config["mongodb"]["username"]
+password = config["mongodb"]["password"]
+app = config["mongodb"]["app"]
 uri = f"mongodb+srv://{username}:{password}@crawled.abmyxjs.mongodb.net/?retryWrites=true&w=majority&appName={app}"
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi("1"))
 
 if __name__ == "__main__":
     try:
-        client.admin.command('ping')
-        db = client['clutch']
-        collection = db['agency']
+        client.admin.command("ping")
     except Exception as e:
         logging.error(f"Error connecting to MongoDB: {str(e)}")
