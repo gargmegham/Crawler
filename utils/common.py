7a21572af334a99cc5662a46c0c4bf49649b3c7d
Megham Garg <gargmegham3@gmail.com>
2024-05-05 15:05:11 +0530

    Refactor generate_possible_emails function in common.py for improved email generation logic

--- utils/common.py
+++ utils/common.py
@@ -1,24 +1,47 @@
-def guess_emails(company_data: dict):
-    emails = {}
-    for company_website in company_data:
-        suffix = company_website.replace("https://", "").replace("http://", "")
-        suffix = suffix.split("/")[0]
-        company_people = company_data[company_website]["company_people"]
-        company_name = company_data[company_website]["company_name"]
-        if suffix.startswith("www."):
-            suffix = suffix[4:]
-        for person in company_people:
-            prefix = None
-            person_name = str(person[0]).strip().lower()
-            person_position = str(person[1]).strip().lower()
-            for word in person_name.split(" "):
-                if word.endswith("."):
-                    continue
-                if word == "linkedin":
-                    break
-                prefix = word
-                break
-            if prefix:
-                email = f"{prefix}@{suffix}"
-                emails[email] = [person_name, person_position, company_name]
-    return emails
+def generate_possible_emails(person_name: str, company_domain: str):
+    try:
+        first_name, last_name = person_name.lower().split()
+    except ValueError:
+        if len(person_name.split()) == 1:
+            first_name = person_name.lower()
+            last_name = ""
+        elif len(person_name.split()) > 2:
+            first_name = person_name.lower().split()[0]
+            last_name = person_name.lower().split()[1]
+        else:
+            first_name = ""
+            last_name = ""
+    first_initial = first_name[0]
+    last_initial = last_name[0]
+    email_variations = [
+        # First name last name
+        f"{first_name}.{last_name}@{company_domain}",
+        f"{first_name}{last_name}@{company_domain}",
+        f"{last_name}.{first_name}@{company_domain}",
+        f"{last_name}{first_name}@{company_domain}",
+
+       # First initial last name
+        f"{first_initial}{last_name}@{company_domain}",
+        f"{first_initial}.{last_name}@{company_domain}",
+        f"{last_name}.{first_initial}@{company_domain}",
+        f"{last_name}{first_initial}@{company_domain}",
+
+        # First name last initial
+        f"{first_name}{last_initial}@{company_domain}",
+        f"{first_name}.{last_initial}@{company_domain}",
+        f"{last_initial}{first_name}@{company_domain}",
+        f"{last_initial}.{first_name}@{company_domain}",
+
+        # First initial last initial
+        f"{first_initial}.{last_initial}@{company_domain}",
+        f"{first_initial}{last_initial}@{company_domain}",
+        f"{last_initial}{first_initial}@{company_domain}",
+        f"{last_initial}.{first_initial}@{company_domain}",
+
+        # only one value
+        f"{first_initial}@{company_domain}",
+        f"{last_initial}@{company_domain}",
+        f"{first_name}@{company_domain}",
+        f"{last_name}@{company_domain}",
+    ]
+    return email_variations
